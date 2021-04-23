b81a84089723cced9ffce79572c107f5dca19084
vvyalikh <>
2021-04-23 21:32:28 +0000

    step4

--- main.py
+++ main.py
@@ -1,20 +1,87 @@
  
 word_list = ["aardvark", "baboon", "camel"]
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+end_of_game = False
 chosen_word = random.choice(word_list)
 display = []
 word_length = len(chosen_word)
+lives = 6
+
 for letter in range(word_length):
     display.append('_')
 print(display)
 
-guess = input('Guess the letter: ').lower()
-
-while '_' in display:
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
-            display[position] = guess
-    print(display)
-print('You win!')
+            display[position] = letter
+    if guess not in chosen_word:
+        lives = lives - 1
+        if lives == 0:
+            end_of_game = True
+            print('You loose')
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
