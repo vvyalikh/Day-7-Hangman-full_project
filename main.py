796f0e416607f601c648e406843545f00babce05
vvyalikh <>
2021-04-24 12:09:14 +0000

    step5

--- main.py
+++ main.py
@@ -69,12 +69,16 @@ print(display)
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"This letter has been named already")
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
+            print(f"Your letter is: {guess}")
             display[position] = letter
     if guess not in chosen_word:
         lives = lives - 1
+        print(f"Your letter is: {guess}. There is no letter {guess} in the word. You loose 1 life.")
         if lives == 0:
             end_of_game = True
             print('You loose')
